d6b4d7b4f3b6eb1f0fc5352340517a76d4f55fb4
Evgeny Cherkasov <ech89899@gmail.com>
2023-03-09 19:23:27 +0300

    chore: Demo app

--- example/lib/home/home_screen.dart
+++ example/lib/home/home_screen.dart
@@ -38,7 +38,7 @@ class HomeScreen extends StatelessWidget {
             children: [
               InfoItem(
                 title: 'Theseus Navigator',
-                description: 'A demo app of Theseus Navigator package.\nversion 0.6.0',
+                description: 'A demo app of Theseus Navigator package.\nversion 0.6.3',
                 isAccentStyle: true,
                 isCentered: true,
                 child: ElevatedButton(
